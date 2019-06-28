80c1433e0d40e12f15e4e67b25eb1f2bcdef3d57
Colin Stearns <cstearns@developers.wyyerd.com>
2019-06-28 14:33:38 -0600

    cleanup test

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,9 +1,24 @@
-#[macro_use] extern crate futures;
-#[macro_use] extern crate nom;
-#[macro_use] extern crate prost_derive;
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate futures;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate nom;
+#[macro_use]
+extern crate prost_derive;
+#[cfg(test)]
+#[macro_use]
+extern crate serde_derive;
 
-#[cfg(test)] #[macro_use] extern crate serde_derive;
+pub use client::{DeserializeMessage, Pulsar};
+pub use connection::{Authentication, Connection};
+pub use connection_manager::ConnectionManager;
+pub use consumer::{Ack, Consumer, ConsumerBuilder, ConsumerState, Message, MultiTopicConsumer};
+pub use error::{ConnectionError, ConsumerError, Error, ProducerError, ServiceDiscoveryError};
+pub use message::proto;
+pub use message::proto::command_subscribe::SubType;
+pub use producer::Producer;
+pub use service_discovery::ServiceDiscovery;
 
 pub mod message;
 mod consumer;
@@ -14,29 +29,63 @@ mod connection_manager;
 mod service_discovery;
 mod client;
 
-pub use error::{Error, ConnectionError, ConsumerError, ProducerError, ServiceDiscoveryError};
-pub use connection::{Connection, Authentication};
-pub use connection_manager::ConnectionManager;
-pub use producer::Producer;
-pub use consumer::{Consumer, ConsumerBuilder, MultiTopicConsumer, ConsumerState, Message, Ack};
-pub use service_discovery::ServiceDiscovery;
-pub use client::{Pulsar, DeserializeMessage};
-pub use message::proto;
-pub use message::proto::command_subscribe::SubType;
-
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+
+    use futures::{Future, future, Stream};
+    use futures_timer::ext::FutureExt;
     use tokio;
-    use futures::{Future, Stream, future};
-    use super::*;
+
     use message::proto::command_subscribe::SubType;
+
+    use crate::client::SerializeMessage;
     use crate::consumer::Message;
     use crate::message::Payload;
-    use crate::client::SerializeMessage;
+    use crate::Error as PulsarError;
+
+    use super::*;
 
     #[derive(Debug, Serialize, Deserialize)]
     struct TestData {
         pub data: String
+    }
+
+    #[derive(Debug)]
+    enum Error {
+        Pulsar(PulsarError),
+        Message(String),
+        Timeout(std::io::Error),
+        Serde(serde_json::Error)
+    }
+
+    impl From<std::io::Error> for Error {
+        fn from(e: std::io::Error) -> Self {
+            Error::Timeout(e)
+        }
+    }
+
+    impl From<PulsarError> for Error {
+        fn from(e: PulsarError) -> Self {
+            Error::Pulsar(e)
+        }
+    }
+
+    impl From<serde_json::Error> for Error {
+        fn from(e: serde_json::Error) -> Self {
+            Error::Serde(e)
+        }
+    }
+
+    impl std::fmt::Display for Error {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            match self {
+                Error::Pulsar(e) => write!(f, "{}", e),
+                Error::Message(e) => write!(f, "{}", e),
+                Error::Timeout(e) => write!(f, "{}", e),
+                Error::Serde(e) => write!(f, "{}", e),
+            }
+        }
     }
 
     impl SerializeMessage for TestData {
@@ -65,11 +114,10 @@ mod tests {
 
         let producer = pulsar.producer::<TestData>();
 
-        let produce = {
-            future::join_all((0..5000).map(|_| {
-                producer.send("test", &TestData { data: "data".to_string() })
-            }))
-        };
+
+        future::join_all(
+            (0..5000).map(|_| producer.send("test", &TestData { data: "data".to_string() }))
+        ).wait().unwrap();
 
         let consumer: Consumer<TestData> = pulsar.consumer()
             .with_topic("test")
@@ -80,23 +128,20 @@ mod tests {
             .wait()
             .unwrap();
 
-        produce.wait().unwrap();
-
-        let mut consumed = 0;
-        let _ = consumer.for_each(move |Message { payload, ack, .. }| {
-            consumed += 1;
-            ack.ack();
-            if let Err(e) = payload {
-                println!("Error: {}", e);
-            }
-            if consumed >= 5000 {
-                println!("Finished consuming");
-                Err(ConsumerError::Connection(ConnectionError::Disconnected))
-            } else {
-                Ok(())
-            }
-        }).wait();
-
-        runtime.shutdown_now().wait().unwrap();
+        let _ = consumer
+            .take(5000)
+            .map_err(|e| PulsarError::Consumer(e).into())
+            .for_each(move |Message { payload, ack, .. }| {
+                ack.ack();
+                let data = payload?;
+                if data.data.as_str() == "data" {
+                    Ok(())
+                } else {
+                    Err(Error::Message(format!("Unexpected payload: {}", &data.data)))
+                }
+            })
+            .timeout(Duration::from_secs(5))
+            .wait()
+            .unwrap();
     }
 }
