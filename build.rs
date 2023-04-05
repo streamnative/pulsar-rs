bedf415b6deb925920f55d480e68557749ba3663
Liam Kinne <liam@kinne.io>
2023-04-05 12:17:18 +1000

    add feature flag

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 extern crate prost_build;
 
 fn main() {
+    #[cfg(feature = "protobuf-src")]
     std::env::set_var("PROTOC", protobuf_src::protoc());
 
     prost_build::compile_protos(&["./PulsarApi.proto"], &["./"]).unwrap();
